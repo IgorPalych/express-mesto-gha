4156e9db13e5ba6791e6dded1a7b41b16a5e7ab6
IgorPalych <mylnx@mail.ru>
2023-05-23 13:15:46 +0300

    ПР-13: feat: создал схемы

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  /* useCreateIndex: true,
+  useFindAndModify: false, */
+});
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порте ${PORT}`);
